4cc3d9351f9f1455456905993c8131d6da904d73
Noah Jorgensen <35749583+Noah-Jorgensen@users.noreply.github.com>
2022-12-04 03:43:13 -0500

    Add files via upload

---
+++ 2022/Day4/Part2/d4p2.kt
@@ -0,0 +1,21 @@
+import java.io.File
+ 
+fun main() {
+    val inFile = "input"
+    val lines: List<String> = File(inFile).readLines()
+
+    var pairs = 0
+    for (item in lines){
+        val new = item.split(",")
+        val p1 = new[0]
+        val p2 = new[1]
+        val one_fh = (p1.split("-")[0]).toInt()
+        val one_sh = (p1.split("-")[1]).toInt()
+        val two_fh = (p2.split("-")[0]).toInt()
+        val two_sh = (p2.split("-")[1]).toInt()
+        if ((one_sh >= two_fh) && (one_fh <= two_sh)){
+            pairs++
+        }
+    }
+    println(pairs)
+}
